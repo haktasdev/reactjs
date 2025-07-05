b602ff58076b4e66933fa789876b8b66dd35f51d
carlos <carlos@gmail.com>
2025-07-06 02:17:24 +0300

    AS

--- src/Companies.js
+++ src/Companies.js
@@ -118,7 +118,7 @@ function Companies() {
 
   return (
     <div style={{ maxWidth: 600, margin: '40px auto', fontFamily: 'Arial, sans-serif' }}>
-      <h1>Merve seni çok seviyorum KALP</h1>
+      <h1>ŞİRKETLER</h1>
 
       <button
         onClick={() => {
